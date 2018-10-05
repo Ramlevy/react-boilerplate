6a35cf0c857a9559d077d812f7e218be1f04c2e8
Ram <ramlevy@gmail.com>
2018-10-05 19:57:56 +0300

    formatting

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => (
   <div>
-    {/* Client side routing to prevent refreshes by getting pages from Server side(using <a> tag for example) */}
+    { /* Client side routing to prevent refreshes by getting pages from Server side(using <a> tag for example) */}
     404 - <Link to="/">Go to homepage</Link>
   </div>
 );
